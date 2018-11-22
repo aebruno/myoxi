b26f4f9d5e84239f82b002206dbbebb0faa25c31
Andrew E. Bruno <aeb@qnot.org>
2018-11-22 13:24:20 -0500

    Compute Oxygen Desaturation Index (ODI) and record desaturation events

--- tools/stats.go
+++ tools/stats.go
@@ -24,7 +24,102 @@ import (
 
 	"github.com/aebruno/myoxi/model"
 	. "github.com/logrusorgru/aurora"
+	log "github.com/sirupsen/logrus"
 )
+
+type DesaturationEvent struct {
+	start   time.Time
+	end     time.Time
+	avg120  float64
+	records []*model.OxiRecord
+}
+
+func (d *DesaturationEvent) String() string {
+	sum := 0
+	for _, rec := range d.records {
+		sum += int(rec.Spo2)
+	}
+	mean := float64(sum) / float64(len(d.records))
+	return fmt.Sprintf("%s lasting %s desaturation %.2f to %.2f", d.start.Format("01-02 15:04:05"), d.end.Sub(d.start), d.avg120, mean)
+}
+
+func computeODI(data []*model.OxiRecord) (float64, []*DesaturationEvent) {
+	nullTime := time.Time{}
+	avg120 := float64(95)
+	sum120 := 0
+	count120 := 1
+
+	sumODI := 0
+	countODI := 0
+
+	events := make([]*DesaturationEvent, 0)
+	curEvent := &DesaturationEvent{}
+
+	idx := 0
+	processHours := true
+
+	for processHours {
+		hourODI := 0
+		end := idx + 3600
+
+		if end > len(data) {
+			end = len(data)
+			processHours = false
+		}
+
+		for _, rec := range data[idx:end] {
+			// Throw away data that's not within reasonable physical limits
+			if rec.Pulse < 40 {
+				continue
+			}
+			if rec.Spo2 < 65 {
+				continue
+			}
+
+			if avg120-float64(rec.Spo2) >= 4 {
+				log.Debugf("Oxygen desaturation event at %s: %d (%.2f 120s avg)", rec.DateTime.Format("01-02 15:04:05"), rec.Spo2, avg120)
+				hourODI++
+				if curEvent.start == nullTime {
+					curEvent.start = rec.DateTime
+					curEvent.records = append(curEvent.records, rec)
+					curEvent.avg120 = avg120
+				} else {
+					curEvent.records = append(curEvent.records, rec)
+				}
+			} else if curEvent.start != nullTime {
+				curEvent.end = rec.DateTime
+				events = append(events, curEvent)
+				curEvent = &DesaturationEvent{}
+			}
+
+			sum120 += int(rec.Spo2)
+			count120++
+
+			if count120 == 120 {
+				avg120 = float64(sum120) / 120
+				count120 = 1
+				sum120 = 0
+				log.Debugf("Avg 120: %.2f", avg120)
+			}
+		}
+
+		idx += 3600
+		sumODI += hourODI
+		countODI++
+	}
+
+	odi := float64(sumODI) / float64(countODI)
+	log.Debugf("Hours: %d, ODI: %.2f sum: %d", countODI, odi, sumODI)
+	log.Debugf("Number of events: %d", len(events))
+	for i, ev := range events {
+		log.Debugf("Event %d: avg: %.2f start: %s end: %s", i, ev.avg120, ev.start, ev.end)
+		for _, rec := range ev.records {
+			log.Debugf("     - Record %s", rec)
+		}
+	}
+
+	return odi, events
+}
 
 func Stats(db model.Datastore) error {
 	var pulseMin, pulseMax, spo2Min, spo2Max uint8
@@ -76,12 +171,20 @@ func Stats(db model.Datastore) error {
 	pulseSD = math.Sqrt(pulseSD / n)
 	spo2SD = math.Sqrt(spo2SD / n)
 
+	odi, events := computeODI(records)
+
 	fmt.Printf("------------------------------------------------------\n")
 	fmt.Printf("Start: %s End: %s\n", records[0].DateTime.Format("2006-01-02 15:04:05"), records[len(records)-1].DateTime.Format("2006-01-02 15:04:05"))
 	fmt.Printf("------------------------------------------------------\n")
 	fmt.Printf("Total Records: %d (n = %d, bad data = %d)\n", len(records), int(n), len(records)-int(n))
 	fmt.Printf("Average SpO2 %%: %.2f (min: %d max: %d sd: %.2f)\n", Bold(Blue(spo2Mean)), spo2Min, spo2Max, spo2SD)
 	fmt.Printf("Average Pulse Rate: %.2f (min: %d max: %d sd: %.2f)\n", Bold(Red(pulseMean)), pulseMin, pulseMax, pulseSD)
+	fmt.Printf("ODI: %.2f\n", Bold(Blue(odi)))
+	fmt.Printf("Oxygen Desaturation Events = %d\n", len(events))
+	fmt.Printf("------------------------------------------------------\n")
+	for _, e := range events {
+		fmt.Printf("%s\n", e)
+	}
 	fmt.Printf("\n")
 
 	return nil
